f113c75df05d7ee80d34a21bb113110300d4850e
kartik132000 <sujeetkumarsen37@gmail.com>
2025-03-20 16:45:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [Name, setName] = useState("");
   return (
-    <div>
+    <div className = "app-container">
         {/* Do not remove the main div */}
+    <input 
+    type = "text"
+    placeholder = "Enter your name"
+    value = {name}
+    onChange = {(e) => setName(e.target.value)}
+   className = "input-field" 
+   />
+     {name && <h1 calssname = "greeting">Hello, {name}!<h1/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
